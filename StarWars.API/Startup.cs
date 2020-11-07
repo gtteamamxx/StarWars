2443711acb7f2c41e4fe256f678e08f9621edf5c
Grzegorz Stanik <gstanik@queris.pl>
2020-11-07 11:41:01 +0100

    feat(api): use newtonsoft.json as default json formatter

--- StarWars.API/Startup.cs
+++ StarWars.API/Startup.cs
@@ -12,13 +12,6 @@ namespace StarWars.API
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -35,6 +28,12 @@ namespace StarWars.API
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                    .AddNewtonsoftJson();
         }
     }
 }
